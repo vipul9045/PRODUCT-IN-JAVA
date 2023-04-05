5ab87437bda3637cfef48a53e1e2601c5d77c807
vipul sharma <129940441+vipul9045@users.noreply.github.com>
2023-04-05 14:49:41 +0530

    Add files via upload

---
+++ cube in java.java
@@ -0,0 +1,10 @@
+package java;
+
+public class cube {
+    public static void main(String[] args) {
+        int a = 5;
+        int c = a*a*a;
+        System.out.println("the cube of " + "a" + c);
+    }
+    
+}
