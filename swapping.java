1f25a4e56d9d127a367a8aace83b74413944233e
vipul sharma <129940441+vipul9045@users.noreply.github.com>
2023-04-05 14:58:30 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,17 @@
+package java;
+
+public class swap {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int temp;
+        System.out.println("before swapping " + "a="+a + "b=" +b);
+        // swap logic
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("after swapping " + "a="+a +"b="+b);
+        
+    }
+    
+}
