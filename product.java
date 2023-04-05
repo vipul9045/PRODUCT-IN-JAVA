2e612269c3ff3e8ee677be83b159e272d0f99fbb
vipul9045 <129940441+vipul9045@users.noreply.github.com>
2023-04-05 13:50:25 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,9 @@
+public class product {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        int product = x*y;
+        System.out.println("The product is "+product);
+    }
+    
+}
