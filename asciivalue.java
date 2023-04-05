1bd578528e69fd31bd0198c5824a90619042f97f
vipul9045 <129940441+vipul9045@users.noreply.github.com>
2023-04-05 14:34:32 +0530

    Add files via upload

---
+++ asciivalue.java
@@ -0,0 +1,13 @@
+package java;
+
+public class asciivalue {
+    public static void main(String[] args) {
+        char c = 'u';
+        int asciiValue = (int) c;
+        System.out.println("The ASCII value of " + c + " is " + asciiValue);
+    }
+    
+}
+
+
+
