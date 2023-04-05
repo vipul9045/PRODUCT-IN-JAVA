64f156ecd346ddeca46130b533056f54c8b5fe42
vipul sharma <129940441+vipul9045@users.noreply.github.com>
2023-04-05 14:44:17 +0530

    Add files via upload

---
+++ rectanglearea.java
@@ -0,0 +1,12 @@
+package java;
+
+public class rectanglearea {
+    public static void main(String[] args) {
+        int length = 7;
+        int breadth = 4;
+        // area of rectangle = length*breadth;
+        int area = length*breadth;
+        System.out.println("The area of rectangle" +area);
+    }
+    
+}
